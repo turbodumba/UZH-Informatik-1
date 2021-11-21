e916ba0319c02df3524939a9626db05ec91d8e57
turbodumba <thomas.www@hotmail.com>
2021-11-21 21:14:07 +0100

    Fridge

---
+++ Access/Restaurant.py
@@ -0,0 +1,60 @@
+from item import Item
+from order import Order
+
+
+class Restaurant:
+
+    def __init__(self, restaurant_name, menu_list):
+        self.__restaurant_name = restaurant_name
+        self.__menu_list = menu_list
+        self.__order_list = []
+
+    def get_restaurant_name(self):
+        return self.__restaurant_name
+
+    def get_menu_list(self):
+        return self.__menu_list
+
+    def get_order_list(self):
+        if len(self.__order_list)==0:
+            return "No order yet"
+        return self.__order_list
+
+    def set_order(self, item_list):
+        actual_order_list = []
+        for i in item_list:
+            if i in self.__menu_list:
+                actual_order_list.append(i)
+        if not len(actual_order_list) == 0:
+            o = Order(actual_order_list)
+            self.__order_list.append(o)
+
+    def get_revenue(self):
+        total_rev = 0
+        for o in self.__order_list:
+            total_rev += o.get_bill_amount()
+        return total_rev
+
+
+# You can play around with your implementation in the body of the following 'if'.
+# The contained statements will be ignored while evaluating your solution.
+if __name__ == '__main__':
+    # Create Item Objects with Name and Price
+    steak = Item("Steak", 25)
+    salad = Item("Salad", 10)
+    fish = Item("Fish", 30)
+    pizza = Item("Pizza", 40)
+    hamburger = Item("Hamburger", 20)
+    # Create menu list
+    menu_list = [hamburger, pizza]
+    # Create order list
+    order_list = [fish]
+    # Create restaurant object with name and menu list
+    restaurant = Restaurant("Zurich_1", menu_list)
+    # Create an order with the order list
+    restaurant.set_order(order_list)
+    # Get the revenue of the restaurant object
+    print(restaurant.get_revenue())
+    print(restaurant.get_restaurant_name())
+    print(restaurant.get_menu_list())
+    print(restaurant.get_order_list())
